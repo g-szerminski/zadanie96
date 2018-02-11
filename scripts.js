635aad314e5d670c72f1a86d6392c93ff36d3b32
Grzegorz Szermiński <szerman@poczta.fm>
2018-02-11 11:00:09 +0100

    Add space between item and number

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ var add = document.getElementById('addElem');
 var nextElem = document.getElementsByTagName('li');
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
-	element.innerHTML += 'item' + nextElem.length;
+	element.innerHTML += 'item ' + nextElem.length;
 	list.appendChild(element);
 });
